20977b050c4438cf41911665ad481bec506ab25f
Ihor Porotikov <sobikashi@gmail.com>
2018-06-09 22:41:19 +0300

    social, tabs and resizeble

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+    $("#humburger").click(function () {
 
+        if ($(".header__menu").is(":hidden"))
+        {
+            $(".header__menu").show(100);
+        }
+        else
+        {
+            $(".header__menu").hide(100);
+        }
+
+    })
 });
